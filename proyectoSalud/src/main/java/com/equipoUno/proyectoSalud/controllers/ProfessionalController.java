09e3db284e2fa2872e98484d45ac0a29d652c913
ffsorgi <103230567+ffsorgi@users.noreply.github.com>
2023-05-15 10:19:55 -0300

    Fix url endpoint.FS

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/ProfessionalController.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/ProfessionalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/professionals")
+@RequestMapping("/api/professionals")
 public class ProfessionalController {
 
     private final ProfessionalService professionalService;
