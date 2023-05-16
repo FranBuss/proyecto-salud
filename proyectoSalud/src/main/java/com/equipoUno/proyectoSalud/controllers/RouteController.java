482b0f5c2873725bf08ae2abb9a8d33160fb5ea8
ffsorgi <103230567+ffsorgi@users.noreply.github.com>
2023-05-16 18:26:07 -0300

    Update query professional form and controller.FS

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/RouteController.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/RouteController.java
@@ -1,16 +1,23 @@
 package com.equipoUno.proyectoSalud.controllers;
 
 
+import com.equipoUno.proyectoSalud.entities.Professional;
 import com.equipoUno.proyectoSalud.enumerations.Specialization;
+import com.equipoUno.proyectoSalud.servicies.ProfessionalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class RouteController {
+
+    @Autowired
+    private  ProfessionalService professionalService;
 
     @GetMapping("/")
     public String home(){
@@ -31,6 +38,11 @@ public class RouteController {
     public String searcher(Model model,  @RequestParam Map<String, String> queryParams){
         Specialization[] specializations = Specialization.values();
         model.addAttribute("specializations",specializations);
+        if(!queryParams.isEmpty()){
+            String specialization = queryParams.get("specialization").toString();
+            List<Professional> professionals = professionalService.searchProfessionalsBySpecialization(specialization);
+            model.addAttribute("professionals",professionals);
+        }
         return "searcher";
     }
 
