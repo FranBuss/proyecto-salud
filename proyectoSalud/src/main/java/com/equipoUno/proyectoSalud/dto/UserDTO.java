59ac07a5cfdb05a7852d80247c8e97adffff9285
ffsorgi <103230567+ffsorgi@users.noreply.github.com>
2023-05-17 19:45:27 -0300

    Update DTO and AuthController.FS

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/dto/UserDTO.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/dto/UserDTO.java
@@ -1,7 +1,5 @@
 package com.equipoUno.proyectoSalud.dto;
 
-import com.equipoUno.proyectoSalud.enumerations.Rol;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -15,13 +13,12 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class UserDTO {
 
-    @JsonIgnore
-    private String id;
     private String name;
+
     private String email;
-    @JsonIgnore
-    private Rol rol;
-    @JsonIgnore
+
     private String password;
+
+    private String confpassword;
 
 }
