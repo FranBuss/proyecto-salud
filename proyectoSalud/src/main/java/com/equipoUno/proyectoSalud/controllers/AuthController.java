a09d03773630082c8bc88780150e6cc9f4071961
ffsorgi <103230567+ffsorgi@users.noreply.github.com>
2023-05-24 13:45:48 -0300

    Change valid for validated.FS

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/AuthController.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/AuthController.java
@@ -11,9 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/api/auth")
@@ -29,7 +28,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String register(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult bindingResult, Model model) {
+    public String register(@Validated @ModelAttribute("userDTO") UserDTO userDTO, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("userDTO", userDTO);
