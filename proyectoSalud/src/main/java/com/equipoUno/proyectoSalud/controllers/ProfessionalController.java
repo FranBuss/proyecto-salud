fb2c50990bacd588b3423ebf47944c5041c89f30
Maxiescobar90 <maxiescobar90@gmail.com>
2023-05-27 13:46:53 -0300

    mejor calidad img al carousel index + seccion noticias

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/ProfessionalController.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/ProfessionalController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/professionals")
+@RequestMapping("896656")
 public class ProfessionalController {
 
     private final ProfessionalService professionalService;
