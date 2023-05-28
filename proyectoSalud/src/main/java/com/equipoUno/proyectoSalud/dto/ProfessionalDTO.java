54b85acc637c0662f071bc6ea9f73beb60270200
ffsorgi <103230567+ffsorgi@users.noreply.github.com>
2023-05-28 18:09:58 -0300

    Add file extension anotation and fix validatacion.FS

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/dto/ProfessionalDTO.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/dto/ProfessionalDTO.java
@@ -4,6 +4,9 @@ import com.equipoUno.proyectoSalud.enumerations.Specialization;
 import lombok.*;
 
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 @Data
@@ -11,9 +14,14 @@ import java.time.LocalTime;
 @AllArgsConstructor
 public class ProfessionalDTO{
 
+    @NotNull(message = "Tiene que seleccionar una especialización válida.")
+    @Enumerated(EnumType.STRING)
     private Specialization specialization;
+
     private LocalTime entryTime;
+
     private LocalTime exitTime;
+
     private float charge;
 
     public String getSpecializationName(){
