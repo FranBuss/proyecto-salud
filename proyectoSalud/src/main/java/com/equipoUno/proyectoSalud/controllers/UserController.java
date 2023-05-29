0e4aca5e4ab6123f921f7a622e41d0f4e0cb0dee
Nicolas Cia <nicoo.c05@gmail.com>
2023-05-29 15:55:59 -0300

    Added automatic update for user data.

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/UserController.java
+++ proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.equipoUno.proyectoSalud.controllers;
 
 import com.equipoUno.proyectoSalud.dto.UserDTO;
+import com.equipoUno.proyectoSalud.entities.User;
 import com.equipoUno.proyectoSalud.exceptions.MiException;
 import com.equipoUno.proyectoSalud.servicies.UserServiceImplement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -25,8 +28,10 @@ public class UserController {
     }
 
     @PostMapping("/update/{id}")
-    public String updateUser(@PathVariable String id, @ModelAttribute("userDTO")UserDTO userDTO) throws MiException {
-        userService.updateUser(id, userDTO);
+    public String updateUser(@PathVariable String id, @ModelAttribute("userDTO") UserDTO userDTO, HttpSession session) throws MiException {
+        User userSession = (User) session.getAttribute("userSession");
+        userSession = userService.updateUser(id, userDTO, userSession);
+        session.setAttribute("userSession", userSession);
         return "redirect:/profile";
     }
 }
